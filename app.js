99f2d84b849f504af5ec5417cfd94660123ee27d
Pierre Vincent Hernandez <pierre_hernandez@dlsu.edu.ph>
2022-04-15 14:17:45 +0800

    Declare static files

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+// Imports
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
 const PORT = 5000;
+
+// Static files
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/img', express.static(__dirname + 'public/img'));
