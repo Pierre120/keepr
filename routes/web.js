19344f09be25dfbd1d76f9c1b48e5f460e04db21
Pierre Vincent Hernandez <pierre_hernandez@dlsu.edu.ph>
2022-04-15 14:36:16 +0800

    Setup routing

---
+++ routes/web.js
@@ -0,0 +1,21 @@
+// Imports
+const express = require('express');
+
+let router = express.Router();
+
+let initRoutes = (app) => {
+  router.get('/', (req, res) => {
+    return res.render('index');
+  }); // Landing page
+
+  router.get('/register'); // Register page
+
+  router.get('/app'); // App/Home page
+
+  router.get('/workspace'); // workspace
+
+  return app.use('/', router); // Set the router
+};
+
+// Exports the function for the app.js to use
+module.exports = initRoutes;
