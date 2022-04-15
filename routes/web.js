ffb54529686a46132c2a26709216226421a5a7e0
Pierre Vincent Hernandez <pierre_hernandez@dlsu.edu.ph>
2022-04-15 14:40:04 +0800

    Add layouts

--- routes/web.js
+++ routes/web.js
@@ -5,7 +5,7 @@ let router = express.Router();
 
 let initRoutes = (app) => {
   router.get('/', (req, res) => {
-    return res.render('index');
+    return res.send('<h1>Hello World!</h1>');
   }); // Landing page
 
   router.get('/register'); // Register page
