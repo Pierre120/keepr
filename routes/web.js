1dede61791464aadd1d8e877023f041eb4189466
Pierre Vincent Hernandez <pierre_hernandez@dlsu.edu.ph>
2022-05-05 21:55:44 +0800

    Rename sampleWorkspaces to workspacePaths

--- routes/web.js
+++ routes/web.js
@@ -68,7 +68,7 @@ const sampleOtherUsers = [
   }
 ];
 
-const sampleWorkspaces = {
+const workspacePaths = {
   default: 'Default Workspace',
   shoppee: 'Shoppee',
   lozoda: 'Lozoda',
@@ -96,7 +96,7 @@ let initRoutes = (app) => {
     return res.render('dashboard', {
       active: 0,
       layout: './layouts/workspace',
-      workspace: sampleWorkspaces[req.params.workspace]
+      workspace: workspacePaths[req.params.workspace]
     })
   }); // dashboard
 
@@ -104,7 +104,7 @@ let initRoutes = (app) => {
     return res.render('inventory', { 
       active: 1,
       layout: './layouts/workspace',
-      workspace: sampleWorkspaces[req.params.workspace],
+      workspace: workspacePaths[req.params.workspace],
       sampleItems: sampleData
     });
   }); // inventory
@@ -113,7 +113,7 @@ let initRoutes = (app) => {
     return res.render('collaborators', {
       active: 2,
       layout: './layouts/workspace',
-      workspace: sampleWorkspaces[req.params.workspace]
+      workspace: workspacePaths[req.params.workspace]
     })
   }); // collaborators
 
@@ -121,7 +121,7 @@ let initRoutes = (app) => {
     return res.render('history', {
       active: 3,
       layout: './layouts/workspace',
-      workspace: sampleWorkspaces[req.params.workspace]
+      workspace: workspacePaths[req.params.workspace]
     })
   }); // history
 
