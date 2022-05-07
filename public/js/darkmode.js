adf7d04d78e9e23ec52a4d2c295d614c925cf0c6
Pierre Vincent Hernandez <pierre_hernandez@dlsu.edu.ph>
2022-05-07 13:35:27 +0800

    Add enableDarkMode() function for enabling dark mode

--- public/js/darkmode.js
+++ public/js/darkmode.js
@@ -3,4 +3,9 @@ let darkMode = localStorage.getItem('darkMode');
 // Get the element responsible for the dark mode toggle
 const darkModeToggle = document.querySelector('#dark-mode-toggle');
 
-
+const enableDarkMode = () => {
+  // Add .dark-mode to body
+  document.body.classList.add('dark-mode');
+  // Update darkMode status in the LocalStorage to `enabled`
+  localStorage.setItem('darkMode', 'enabled');
+};
