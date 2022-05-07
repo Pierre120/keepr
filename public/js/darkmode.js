e94d8a4405dbfc7d499de85e8b71e29219bc5e47
Pierre Vincent Hernandez <pierre_hernandez@dlsu.edu.ph>
2022-05-07 13:32:09 +0800

    Add darkMode for retreiving the current theme and darkModeToggle for the toggle

---
+++ public/js/darkmode.js
@@ -0,0 +1,6 @@
+// Retrieve the file containing the info if dark mode is enabled
+let darkMode = localStorage.getItem('darkMode');
+// Get the element responsible for the dark mode toggle
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+
