7d5833ede408fa400ea1e5c3380013c47a066d4a
Pierre Vincent Hernandez <pierre_hernandez@dlsu.edu.ph>
2022-05-31 21:57:20 +0800

    Add controllers/user-controller.js

---
+++ controllers/user-controller.js
@@ -0,0 +1,31 @@
+const User = require('../database/models/User.js');
+
+// For adding new user to the database
+const createUser = (req, res) => {
+
+};
+
+// For editing info of user
+const editUser = (req, res) => {
+
+};
+
+// For deleting a user in a database
+const deleteUser = (req, res) => {
+
+};
+
+// For viewing user account
+const viewUser = (req, res) => {
+
+};
+
+// For finding a user using his/her username
+const findUserByUsername = (req, res) => {
+
+};
+
+// for finding a user using his/her display name
+const findUserByDisplayName = (req, res) => {
+
+};
