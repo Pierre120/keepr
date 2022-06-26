bde42767de8f316b7072603487f8e16daeb609fa
Pierre Vincent Hernandez <pierre_hernandez@dlsu.edu.ph>
2022-06-26 18:07:36 +0800

    Initialize register-controller.js

---
+++ controllers/register-controller.js
@@ -0,0 +1,32 @@
+const User = require('../database/models/User.js');
+const bcrypt = require('bcrypt');
+
+// For creating new user
+const createNewUser = async (req, res) => {
+  // Hash password
+  let hashedPwd = await bcrypt.hash(req.body.password, 10);
+  // Create User object
+  const newUser = new User({
+    username: req.body.username,
+    displayName: req.body.displayName,
+    password: hashedPwd
+  });
+
+  try {
+    await user.save();
+    res.send(true); // Registered successfully
+  } catch(err) {
+    console.log(err);
+    res.send(false); // Failed to register
+  }
+};
+
+// For getting the `Register` page
+const viewRegisterPage = async (req, res) => {
+  res.render('register', { layout: './layouts/register-page' });
+};
+
+module.exports = {
+  createNewUser: createNewUser,
+  viewRegisterPage: viewRegisterPage
+}
