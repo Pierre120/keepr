2671ffd2456b8b62133a79c25ea36c861778e88e
Thread <airon_cruz@dlsu.edu.ph>
2022-06-27 22:04:42 +0800

    added contents for viewHistoryPage callback fucntion

--- controllers/history-controller.js
+++ controllers/history-controller.js
@@ -2,7 +2,7 @@ const User = require('../database/models/History.js');
 const bcrypt = require('bcrypt');
 
 const viewHistoryPage = async(req,res) =>{
-    
+    res.render('history', {layout: './layouts/workspace-page'});
 };
 
 const clearHistory = async(req,res) => {
