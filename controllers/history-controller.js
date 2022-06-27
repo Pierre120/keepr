82a293875909b1311bec824be90a68e1763e9b6c
Thread <airon_cruz@dlsu.edu.ph>
2022-06-27 21:58:17 +0800

    added functions for historyController.js

--- controllers/history-controller.js
+++ controllers/history-controller.js
@@ -1,2 +1,20 @@
 const User = require('../database/models/History.js');
 const bcrypt = require('bcrypt');
+
+const viewHistoryPage = async(req,res) =>{
+    
+};
+
+const clearHistory = async(req,res) => {
+
+};
+
+const sortHistory = async(req,res) => {
+
+};
+
+module.exports = {
+    viewHistoryPage: viewHistoryPage,
+    clearHistory: clearHistory,
+    sortHistory: sortHistory
+}
