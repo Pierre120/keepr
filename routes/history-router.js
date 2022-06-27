da0144136e57f6e666a0cf498eff330e4b12cc5d
Thread <airon_cruz@dlsu.edu.ph>
2022-06-27 21:46:13 +0800

    added routes for historyRouter.js

--- routes/history-router.js
+++ routes/history-router.js
@@ -2,3 +2,9 @@ const express = require('express');
 const registerController = require('../controllers/history-controller.js');
 const router = express.Router();
 
+router.get('/', historyController.viewHistoryPage);
+router.post('/clear', historyController.clearHistory);
+router.post('/sort-history', historyController.sortHistory)
+
+
+module.exports = router;
