0d189f21258d45825052e9063546cdb92e6fb64b
Thread <airon_cruz@dlsu.edu.ph>
2022-06-29 18:48:29 +0800

    edited and changed the name of constant controller variable for history controller

--- routes/history-router.js
+++ routes/history-router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const registerController = require('../controllers/history-controller.js');
+const historyController = require('../controllers/history-controller.js');
 const router = express.Router();
 
 router.get('/', historyController.viewHistoryPage);
