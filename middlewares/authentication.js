1b61c17e2e3165b2e9cf0e67fa6976b14febea8e
Pierre Vincent Hernandez <pierre_hernandez@dlsu.edu.ph>
2022-06-29 19:27:28 +0800

    Add authentication middleware

---
+++ middlewares/authentication.js
@@ -0,0 +1,18 @@
+
+// To check if the user is logged in
+const isLoggedIn = (req, res, next) => {
+  if(req.session.isAuth) {
+    next();
+  } else {
+    res.redirect('/');
+  }
+};
+
+// To check if the user is not logged in
+const isNotLoggedIn = (req, res, next) => {
+  if(req.session.isAuth) {
+    res.redirect('/app');
+  } else {
+    next();
+  }
+}
