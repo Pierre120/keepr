d8f9fb14d07a079482639e707d8c67831466cae0
Thread <airon_cruz@dlsu.edu.ph>
2022-06-29 23:30:56 +0800

    added historyRouter within the mainRouter.js

--- routes/main-router.js
+++ routes/main-router.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const tempRouter = require('./web.js');
 const regRouter = require('./register-router.js');
+const historyRouter = require('./history-router.js');
 
 // Main router
 let router = express.Router();
@@ -12,6 +13,7 @@ const initRoutes = (app) => {
   // Routing on main path
   app.use('/', tempRouter); // use web.js as a temporary router
   app.use('/register', regRouter); // Register Page
+  app.use('/:workspace/history', historyRouter);
 };
 
 // Exports the function for the app.js to use
