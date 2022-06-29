ead661ed5e2086752f4d8f54ac724b16bbbf13f6
Pierre Vincent Hernandez <pierre_hernandez@dlsu.edu.ph>
2022-06-29 23:16:33 +0800

    Add subprogram sendAlerts() for sending alerts to the client

--- controllers/loginlogout-controller.js
+++ controllers/loginlogout-controller.js
@@ -12,44 +12,40 @@ const loginUser = async (req, res) => {
   const {username, password} = req.body;
   // Find the user in the db given the username
   const user = await User.findOneByUsername(username);
-  const errMsg = 'Invalid username and/or password';
 
   // Checks if user exists
   if(!user) {
     // Sends the alert to user
-    return res.render('partials/alert', {
-      type: 'warning',
-      message: errMsg
-    }, (err, html) => {
-      if(err) {
-        console.log(err);
-        res.send(false);
-      } else {
-        res.send({ isErr: 1, content: html })
-      }
-    });
+    return sendAlert(res);
   }
 
   // Checks if the given password is a match
   const isMatch = await bcrypt.compare(password, user.password);
   if(!isMatch) {
     // Sends the alert to user
-    return res.render('partials/alert', {
-      type: 'warning',
-      message: errMsg
-    }, (err, html) => {
-      if(err) {
-        console.log(err);
-        res.send(false);
-      } else {
-        res.send({ isErr: 1, content: html });
-      }
-    });
+    return sendAlert(res);
   }
 
   // Authenticate and add session
   req.session.isAuth = true;
   res.send({ isErr: 0, content: '/app' }); // redirect to the home page
+}
+
+// for sending alerts
+const sendAlert = (res) => {
+  const errMsg = 'Invalid username and/or password';
+
+  res.render('partials/alert', {
+    type: 'warning',
+    message: errMsg
+  }, (err, html) => {
+    if(err) {
+      console.log(err);
+      res.send(false);
+    } else {
+      res.send({ isErr: 1, content: html });
+    }
+  });
 }
 
 // For logging out users
