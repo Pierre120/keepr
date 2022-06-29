f19c643c888fc4f2d1883d04aea6591646bc5f6d
Thread <airon_cruz@dlsu.edu.ph>
2022-06-29 22:38:39 +0800

    added dashboardRouter to the mainRouter.js

--- routes/main-router.js
+++ routes/main-router.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const tempRouter = require('./web.js');
 const regRouter = require('./register-router.js');
+const dashboardRouter = require('./dashboard-router.js');
 
 // Main router
 let router = express.Router();
@@ -12,6 +13,7 @@ const initRoutes = (app) => {
   // Routing on main path
   app.use('/', tempRouter); // use web.js as a temporary router
   app.use('/register', regRouter); // Register Page
+  app.use('/:workspace/dashboard', dashboardRouter);
 };
 
 // Exports the function for the app.js to use
