c4c6a6583dd930531e7057f7ee120cfca8b7f19b
Pierre Vincent Hernandez <pierre_hernandez@dlsu.edu.ph>
2022-06-30 15:21:44 +0800

    Add and initialize controllers/user-controller.js

---
+++ controllers/user-controller.js
@@ -0,0 +1,24 @@
+const User = require('../models/User.js');
+
+const viewAccountPage = (req, res) => {
+
+}
+
+const updateUserInfo = (req, res) => {
+
+}
+
+const deleteUser = (req, res) => {
+
+}
+
+const updateProfilePic = (req, res) => {
+
+}
+
+module.exports = {
+  viewAccountPage: viewAccountPage,
+  updateUserInfo: updateUserInfo,
+  deleteUser: deleteUser,
+  updateProfilePic: updateProfilePic
+}
