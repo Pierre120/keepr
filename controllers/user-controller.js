3f5e1c0df110a0478d61e1f6b693ef3ec32a8a96
Pierre Vincent Hernandez <pierre_hernandez@dlsu.edu.ph>
2022-07-02 01:36:35 +0800

    Add definition to updateUserInfo() function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -18,12 +18,31 @@ const viewAccountPage = async (req, res) => {
   });
 }
 
-const updateUserInfo = (req, res) => {
-
+const updateUserInfo = async (req, res) => {
+  try {
+    // Retrive record of requesting user
+    const user = await User.findById(req.session.user);
+  
+    // Check if user exist and the 
+    // requesting user is the same user being edited
+    if(!user || user.username !== req.body.owner) {
+      // Redirect back to the account page of the user
+      return res.redirect(400, '/user' + req.params.userId);
+    }
+  
+    // Apply edits or updates
+    user[req.body.property] = req.body[req.body.property];
+    await user.save(); // save the changes
+    res.redirect(200, '/user' + req.params.userId);
+  } catch (err) {
+    // Error encountered
+    console.log(err);
+    res.redirect(500, '/user' + req.params.userId);
+  }
 }
 
 const deleteUser = (req, res) => {
-
+  
 }
 
 const updateProfilePic = (req, res) => {
