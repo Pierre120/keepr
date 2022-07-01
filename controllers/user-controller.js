234e5faf1facbebc8bb325bd270459dc349606f9
Pierre Vincent Hernandez <pierre_hernandez@dlsu.edu.ph>
2022-07-02 00:49:32 +0800

    Finish initializing viewAccountPage() function for viewing the user's account page

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,12 +1,21 @@
 const User = require('../models/User.js');
 
+// For viewing the account page of a user
 const viewAccountPage = async (req, res) => {
   // retrieve user info to be viewed
   const viewUser = await User.findOneByUsername(req.params.username);
   // determine if the owner is viewing his/her account page
   const isOwner = (req.session.user === viewUser._id) ? true : false;
 
-  
+  // Render the account page of the user being viewed
+  res.render('account', {
+    layout: './layouts/account-page',
+    user: viewUser,
+    isInWorkspace: false,
+    deleteType: 'account',
+    hasEditModal: true,
+    isOwner: isOwner
+  });
 }
 
 const updateUserInfo = (req, res) => {
