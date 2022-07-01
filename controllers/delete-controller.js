9d063886817dcce7c0cdaf6c8afd8cb4b6222e24
Thread <airon_cruz@dlsu.edu.ph>
2022-07-01 18:49:37 +0800

    redirects to home page

--- controllers/delete-controller.js
+++ controllers/delete-controller.js
@@ -3,7 +3,7 @@ const Workspace = require('../models/Workspace.js');
 const deleteWorkspace = async (req,res) =>{
     var workspace = req.body.property;
     Workspace.findByIdAndDelete(workspace);
-    res.send({isErr:0, content:'/'});
+    res.send({isErr:0, content:'/app'});
 };
 
 module.exports ={
