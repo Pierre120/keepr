a64db94880beab6fefdf2f8da8456b73de212c22
Thread <airon_cruz@dlsu.edu.ph>
2022-07-01 18:44:25 +0800

    deletes the workspace using the the deleteWorkspace function

--- controllers/delete-controller.js
+++ controllers/delete-controller.js
@@ -1,7 +1,9 @@
 const Workspace = require('../models/Workspace.js');
 
 const deleteWorkspace = async (req,res) =>{
-    
+    var workspace = req.body.property;
+    Workspace.findByIdAndDelete(workspace);
+    res.send({isErr:0, content:'/'});
 };
 
 module.exports ={
