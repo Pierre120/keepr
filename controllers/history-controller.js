9696adc5be7b8c9740da0a1d9b119150ec68efa3
Thread <airon_cruz@dlsu.edu.ph>
2022-07-01 22:53:15 +0800

    filled the clearHistory function

--- controllers/history-controller.js
+++ controllers/history-controller.js
@@ -1,11 +1,23 @@
 const History = require('../database/models/History.js');
+const Workspace = require('../database/models/Workspace.js');
 
 const viewHistoryPage = async(req,res) =>{
     res.render('history', {layout: './layouts/workspace-page'});
 };
 
 const clearHistory = async(req,res) => {
-
+    try{
+        var targetHistory = await Workspace.findById(req.body.workspace);
+        var history_arr = targetHistory.history;
+        for(var i=0; i<history_arr.length; i++)
+        {
+            History.findByIdAndDelete(history_arr[i]);
+        }
+        res.redirect('/app');
+    }catch(err){
+        console.log(err);
+        res.redirect('back');
+    }
 };
 
 const sortHistory = async(req,res) => {
