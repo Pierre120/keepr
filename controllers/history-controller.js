031634e3b3941512607e9c8d3af1424349648a14
Thread <airon_cruz@dlsu.edu.ph>
2022-07-03 04:16:12 +0800

    added content for viewHistoryPage function
    *Passing the current workspace object retrieved from MongoDB
    *Passing the array of history object array of the current workspace of the user

--- controllers/history-controller.js
+++ controllers/history-controller.js
@@ -2,7 +2,29 @@ const History = require('../database/models/History.js');
 const Workspace = require('../database/models/Workspace.js');
 
 const viewHistoryPage = async(req,res) =>{
-    res.render('history', {layout: './layouts/workspace-page'});
+    const workspace = await Workspace.findById(req.params.workspace);
+    const history = workspace.history;
+    let histor_arr = [];
+    for(var i=0; i<history.length; i++)
+    {
+        history_arr[i] = await History.findById(history[i]);
+    }
+    res.render('history', {
+        active: 3,
+        layout: './layouts/workspace-page',
+        isInWorkspace: true,
+        deleteType: 'history',
+        hasAddModal: false,
+        hasEditModal: false,
+        hasSortModal: 'history',
+        // TODO: Pass the `Workspace` object of the current user retrieved from mongodb
+        workspace: workspace,
+        // TODO: Pass the array of history records of the current workspace.
+        // NOTE: Make the Workspace's `history` property array into an object array.
+        //      Since this properties contains only the ObjectId and it will not render itself in the frontend.
+         history: history_arr
+    
+      })
 };
 
 const clearHistory = async(req,res) => {
@@ -34,7 +56,9 @@ const sortHistory = async(req,res) => {
                 history_sorted = await History.find({_id: {$in: targetHistory.history}})
                                             .sort({editDate: 1}); //ascending order
                 break;
-            case "DESC": history_sorted = await History.find({_id: {$in: targetHistory.history}}).sort({editDate: -1}); //descending order
+            case "DESC": 
+                history_sorted = await History.find({_id: {$in: targetHistory.history}})
+                                            .sort({editDate: -1}); //descending order
             break;
         }
         res.redirect('/:workspace/history');
