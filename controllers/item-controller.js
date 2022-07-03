ff9236fb84d284aca3fe2cda610e6a7950ec8cd8
Pierre Vincent Hernandez <pierre_hernandez@dlsu.edu.ph>
2022-07-03 13:19:23 +0800

    Fix error in updateItemInfo() function in item-controller.js

--- controllers/item-controller.js
+++ controllers/item-controller.js
@@ -24,25 +24,23 @@ const viewItemPage = async (req, res) => {
 };
 
 const updateItemInfo = async (req, res) => {
+	// Get item
+	const item = await Item.findOneByProductCode(req.params.pcode);
   try {
-		// Get item
-    const item = await Item.findOneByProductCode(req.params.pcode);
+		console.log(`=== Item being modified:  ${item._id} (${item.itemName})`)
     // Apply edits or updates
-		// ISSUE: changing itemName doesn't work when pcode is still the same
     item[req.body.property] = req.body[req.body.property];
-
-    console.log(`Item property being updated: ${req.body.property}`);
+    console.log(`=== Item property being updated: ${req.body.property}`);
 
     // save the changes
     await item.save(); 
-
-    console.log(`Updated item: ${item}`);
-
-    res.status(200).redirect('/' + req.params.workspace + '/' + req.params.pcode);
+    console.log(`=== Updated item: ${item}`);
+		// Refresh the item page
+    res.status(200).redirect('/' + req.params.workspace + '/' + item.pcode);
   } catch (err) {
     // Error encountered
     console.log(err);
-    res.status(500).redirect('/' + req.params.workspace + '/' + req.params.pcode);
+    res.status(500).redirect('/' + req.params.workspace + '/' + item.pcode);
   }
 }
 
@@ -54,14 +52,16 @@ const deleteItem = async (req, res) => {
 		const item = await Item.findOneByProductCode(req.params.pcode);
 		// Delete from Inventory in current Workspace
 		const index = currWorkspace.inventory.indexOf(item._id);
+		console.log(`=== Item to be deleted:  ${item._id} (${item.itemName})`);
 		// Delete item in workspace inventory
 		currWorkspace.inventory.splice(index, 1);
 		await currWorkspace.save(); // Save changes in `currWorkspace`
-		console.log('===currworkspace inventory spliced===' + currWorkspace.inventory);
-		console.log('====item id of item to delete===' + item._id);
+		console.log(`=== Item, ${item._id} (${item.itemName}) removed`);
+		console.log(`=== from workspace, ${currWorkspace.name} (${currWorkspace._id})`);
 
     // Delete the item document
     await Item.findByIdAndDelete(item._id);
+		console.log(`=== Deleted item:  ${item._id} (${item.itemName})`)
 		// Redirect to inventory page (tentative)
 		res.status(200).redirect('/' + req.params.workspace + '/inventory');
 	} catch (err) {
